b8d8445959d9639c34bd00624ce25bb37c0061cd
LeeHanEum <LeeHanEum160924@outlook.kr>
2022-04-12 10:37:08 +0900

    test on macbook

--- CheckID.py
+++ CheckID.py
@@ -1,6 +1,17 @@
 import re
-GetID = str(input("이이디를 입력하시오 : "))
+New_ID = input("이이디를 입력하시오 : ")
+New_ID = New_ID.lower()
+New_ID = re.sub('[^a-z0-9-_.]', '', New_ID)
+New_ID = New_ID.replace("..",".")
+New_ID = New_ID.replace(New_ID.startswith('.'),"")
+New_ID = New_ID.replace(New_ID.endswith('.'),"")
 
+
+print(New_ID)
+
+
+
+'''
 if len(GetID) > 15 :
     print("아이디 길이는 15자 이하여야 함")
 elif re.search('[0-9+]',GetID) is None :
@@ -13,10 +24,12 @@ else :
     print("적합한 아이디")
 
 
+
 print("추천 아이디 : ")
 print(GetID.lower())
 print("git이 동작함?")
 print("git이 동작함?")
+'''
 
 
 
