d1f01754b80823b64b5a82ce8a9646b3621e2050
LeeHanEum <LeeHanEum160924@outlook.kr>
2022-05-20 08:59:48 +0900

    05201

--- C_language/Cpractice4.c
+++ C_language/Cpractice4.c
@@ -25,21 +25,47 @@ bool check(int prices[], size_t prices_len){
 int solution(int prices[], size_t prices_len) {
 
     bool b1 = check(prices, prices_len);
-
-    int answer;
-    int i;
-
-    // for ( i = 0; i < prices_len; i++)
-    // {
-    //     printf("%d ",prices[i]);
-    // }
+    
+    int *answer = (int*)malloc(sizeof(int)*prices_len);
+    int i,j;
     
 
-    return answer;
+    for ( i = 0; i < prices_len; i++)
+    {
+        int c = 0;
+        if (i==prices_len-1)
+        {
+            answer[i] = 0;
+            break;
+        }
+        
+        for ( j = i+1; j < prices_len; j++)
+        {
+            if (prices[i]<=prices[j])
+            {
+                c++;
+            }else{
+                c++;
+                goto finish;
+            }
+            
+        }
+        finish : answer[i] = c;
+        
+    }
+
+    for ( i = 0; i < prices_len; i++)
+    {
+        printf("%d ", answer[i]);
+    }
+    
+
+    return *answer;
 }
 
-int main (){
-    int prices[5] = {1, 2, 3, 2, 3};
+int main(){
+    // int prices[5] = {1,2,3,2,3};
+    int prices[6] = {5,8,6,2,4,1};
     solution(prices, sizeof(prices)/sizeof(int));
     return 0;
 }
